f7d949576a9520d55cd7467ba3c83aa4fd067f57
ashuacharya <ashuacharya622@gmail.com>
2022-07-15 15:30:10 +0545

    footer done, only cart remaining

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import fb from "../Assets/icons/facebook.svg";
+import insta from "../Assets/icons/instagram.svg";
+import github from "../Assets/icons/github.svg";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact__container" id="contact">
+      <div className="contact__content ">
+        <div className="contact__content__text ml2 mr2">Contact us</div>
+        <form className="contact__content__form">
+          <div className="contact__content__form__upper ml2">
+            <li>
+              Name
+              <input type="text" />
+            </li>
+            <li>
+              Email
+              <input type="email" />
+            </li>
+            <li>
+              Message
+              <input type="text" />
+            </li>
+            <button>Contact us</button>
+          </div>
+          <div className="contact__content__form__lower ml2">
+            <div className="contact__content__form__lower__text">
+              Reach us at
+            </div>
+            <div className="contact__content__form__lower__email">
+              ashuacharya622@gmail.com
+            </div>
+            <hr />
+            <div className="contact__content__form__lower__links">
+              <a href="https://www.facebook.com/iamashuacharya/">
+                <img src={fb} alt="" />
+              </a>
+              <a href="https://www.instagram.com/iamashuacharya/">
+                <img src={insta} alt="" />
+              </a>
+              <a href="https://github.com/ashuacharya123">
+                <img src={github} alt="" />
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import logo from "../Assets/icons/logo-footer.svg";
+import fb from "../Assets/icons/facebook.svg";
+import insta from "../Assets/icons/instagram.svg";
+import github from "../Assets/icons/github.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer__container">
+      <div className="footer__content">
+        <div className="footer__content__upper">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="footer__content__middle">
+          <div className="footer__content__middle ml2">
+            <div className="footer__content__middle__text">Reach us at</div>
+            <div className="footer__content__middle__email">
+              ashuacharya622@gmail.com
+            </div>
+            <hr />
+            <div className="footer__content__middle__links">
+              <a href="https://www.facebook.com/iamashuacharya/">
+                <img src={fb} alt="" />
+              </a>
+              <a href="https://www.instagram.com/iamashuacharya/">
+                <img src={insta} alt="" />
+              </a>
+              <a href="https://github.com/ashuacharya123">
+                <img src={github} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="footer__content__lower ml2">
+          <div className="footer__content__lower__text">
+            <span>Subscribe </span>for regular updates
+          </div>
+          <li>
+            Email
+            <input type="email" />
+          </li>
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
