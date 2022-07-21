3d19a3a273ec25a2e8179de546e424e77aadca70
ashuacharya <ashuacharya622@gmail.com>
2022-07-21 17:16:27 +0545

    fixed bug in details section

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -7,12 +7,24 @@ const Details = () => {
     <div className="details__container" id="explore">
       <div className="details__container__content">
         <div className="details__container__content__text ml2 mr2">
-          We handpick tea leaves for the best quality
+          <div>We handpick tea leaves for the best quality</div>
+          <div
+            className="details__container__content__paragraph ml2 mr2"
+            id={window.visualViewport.width > 756 ? "" : "dn"}
+          >
+            Our own scientifically gardened tea garden,where the tea plants are
+            grown organically without any chemical fertilizer leaves you with
+            the best taste across the globe.
+          </div>
         </div>
+
         <div className="details__container__content__image ml2">
           <img src={garden} alt="" />
         </div>
-        <div className="details__container__content__paragraph ml2 mr2">
+        <div
+          className="details__container__content__paragraph ml2 mr2"
+          id={window.visualViewport.width < 756 ? "" : "dn"}
+        >
           Our own scientifically gardened tea garden,where the tea plants are
           grown organically without any chemical fertilizer leaves you with the
           best taste across the globe.
